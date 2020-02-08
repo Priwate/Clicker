ba59673da04c63bb59bd5157c8835f9cfd83bbaf
Algirdas Saliamonas <saliamonasalgirdas@gmail.com>
2020-02-08 02:08:22 +0200

    Inventory buttons(not completed)

--- src/screens/InventoryScreen.js
+++ src/screens/InventoryScreen.js
@@ -8,15 +8,50 @@ class InventoryScreen extends React.Component {
         this.state={
             axe: null,
             pickaxe: null,
-            sword: null
+            sword: null,
+            
             
         }
       }
 
     render(){
         return(
-            <View>
-                <Text>Hello</Text>
+            <View style={styles.buttonlayout}>
+
+                <View style={styles.chopButton}>
+                 <TouchableOpacity
+                 style={styles.buttonSpace}>
+                    <Text>Axe</Text>
+                  </TouchableOpacity>
+                </View>
+
+                <View style={styles.inventoryMiddleLayout}>
+                  <View style={styles.swordButton}>
+                    <TouchableOpacity
+                    style={styles.buttonSpace}>
+                      <Text>Helm</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={{...styles.swordButton, height:'50%'}}>
+                    <TouchableOpacity
+                    style={styles.buttonSpace}>
+                      <Text>Chest</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.swordButton}>
+                    <TouchableOpacity
+                    style={styles.buttonSpace}>
+                      <Text>Helm</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+
+                <View style={styles.adventureButton}>
+                  <TouchableOpacity>
+                    <Text>Sword</Text>
+                  </TouchableOpacity>
+                </View>
+                
             </View>
         )
     }
@@ -29,6 +64,50 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center'
     },
+    buttonlayout: {
+      height: '50%',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      paddingTop: 20, 
+      marginTop: 15
+    },
+    chopButton: {
+      height: 100,
+      width: 100,
+      backgroundColor: 'burlywood',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 16,
+    },
+    inventoryMiddleLayout: {
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+      borderRadius: 16,
+      flexDirection: 'column'
+    },
+    adventureButton: {
+      height: 100,
+      width: 100,
+      backgroundColor: 'gold',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 16
+    },
+    swordButton: {
+      height: 100,
+      width: 100,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'grey',
+      borderRadius: 16,
+      marginBottom: 15
+    },
+    buttonSpace:{
+      height: '100%',
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
   });
 
 export default InventoryScreen;
